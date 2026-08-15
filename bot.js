a739cf13d0753f6cad718c2aeb921b29b629758b
tr4m4d01 <niqsc@yandex.ru>
2026-08-15 20:28:14 +0300

    Add bot

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api");
-require("dotenv").config();
 
-const TOKEN = process.env.BOT_TOKEN;
-const MINI_APP_URL = process.env.MINI_APP_URL;
+const TOKEN = "8618742398:AAGWL2fS8kBYcSC4fHtpeo68sm1u320T7Gg";
+const MINI_APP_URL = "https://tver-benches.onrender.com/";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const user = msg.from;
 
   const keyboard = {
     reply_markup: {
@@ -21,25 +19,9 @@ bot.onText(/\/start/, (msg) => {
 
   bot.sendMessage(
     chatId,
-    "🪑 *Скамейки Твери*\n\n" +
-      "Привет, " +
-      user.first_name +
-      "!\n\n" +
-      "Нажмите кнопку ниже, чтобы открыть карту скамеек.",
+    "🪑 *Скамейки Твери*\n\n" + "Нажмите кнопку ниже, чтобы открыть карту!",
     { parse_mode: "Markdown", ...keyboard },
   );
 });
 
-bot.onText(/\/help/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "ℹ️ *Помощь*\n\n" +
-      "/start - Начать\n" +
-      "/help - Помощь\n\n" +
-      'Нажмите кнопку "🪑 Открыть карту скамеек" для запуска',
-    { parse_mode: "Markdown" },
-  );
-});
-
-console.log("🤖 Бот запущен");
+console.log("Бот запущен");
